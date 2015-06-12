d7ddbbc511ac5b4453acbd21553d5a69dcaea940
Venkata Ravichandra Cherukuri <vcheruk2@ncsu.edu>
2015-06-12 13:30:44 -0400

    Update SubDomainFinder.py

--- SubDomainFinder.py
+++ SubDomainFinder.py
@@ -14,7 +14,7 @@ response = os.system("ping -c 1 " + hostname)
 
 if response == 0:
 	while x == True:
-		for i in xrange(97,123):                              
+		for i in xrange(97,123):
 			test = os.system("ping -c 1 "+chr(i)+"."+hostname)
 			if test == 0:
 				lst.append(chr(i)+"."+hostname)
@@ -26,7 +26,7 @@ if response == 0:
 			for j in xrange(97,123):
 				test = os.system("ping -c 1 "+chr(i)+chr(j)+"."+hostname)
 				if test == 0:
-					lst.append(chr(i)+"."+hostname)
+					lst.append(chr(i)+chr(j)+"."+hostname)
 					ctr += 1
 		x = False
 	x = True
@@ -36,8 +36,19 @@ if response == 0:
 				for k in xrange(97,123):
 					test = os.system("ping -c 1 "+chr(i)+chr(j)+chr(k)+"."+hostname)
 					if test == 0:
-						lst.append(chr(i)+"."+hostname)
+						lst.append(chr(i)+chr(j)+chr(k)+"."+hostname)
 						ctr += 1
+		x = False
+	x = True
+	while x == True:
+		for i in xrange(97,123):
+			for j in xrange(97,123):
+				for k in xrange(97,123):
+					for l in xrange(97,123):
+						test = os.system("ping -c 1 "+chr(i)+chr(j)+chr(k)+chr(l)+"."+hostname)
+						if test == 0:
+							lst.append(chr(i)+chr(j)+chr(k)+chr(l)"."+hostname)
+							ctr += 1
 		x = False
 else:
 	print "Top level Hostname is down, Exiting the Program"
